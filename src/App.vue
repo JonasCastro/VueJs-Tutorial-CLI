59f2f9846556afa063fae7140c06004f90f2028d
Jonas Castro <jonascastro128@gmail.com>
2020-08-16 16:32:50 -0300

    Nesting Components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
 <div>
     <h1>{{ title }}</h1>
-    <p>{{greeting()}}</p>
+    <ninjas></ninjas>
 </div>
 </template>
 
 <script>
+
+import Ninjas from './Ninjas.vue'
+
+
 export default {
+  components:{
+    'ninjas':Ninjas
+  },
   name: 'app',
   data () {
     return {
-      title: 'Hello',
-    }
-  },
-  methods:{
-    greeting: function(){
-        return  'World'
-
+      title: 'Ninja App',
     }
   }
 }
---
+++ src/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+<ul>
+    <li v-for="ninja in ninjas" v-bind:key='ninja'>{{ ninja }}</li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      ninjas:['Yoshi','Mario', 'Ryu'],
+    }
+  }
+}
+</script>
+
+<style>
+</style>
