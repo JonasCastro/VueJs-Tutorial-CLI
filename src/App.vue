50e8b748b6c104589d937a2a5638d5f2e420d7d2
Jonas Castro <jonascastro128@gmail.com>
2020-08-16 16:05:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h1>{{ title }}</h1>
+    <p>{{greeting()}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Hello',
+    }
+  },
+  methods:{
+    greeting: function(){
+        return  'World'
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
