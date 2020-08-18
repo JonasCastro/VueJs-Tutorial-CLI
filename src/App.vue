05fc2302f191dcf4d83e9cf5e13a8a1abe69898d
Jonas Castro <jonascastro128@gmail.com>
2020-08-18 16:20:25 -0300

    props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <template>
-<div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
-</div>
+    <div>
+        <app-header v-bind:title="title"></app-header>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+        <ul>
+          <li v-for="ninja in ninjas">{{ ninja.name }}</li>
+        </ul>
+        <app-footer v-bind:title="title"></app-footer>
+    </div>
 </template>
 
 <script>
-
-import Ninjas from './Ninjas.vue'
-
-
+// Imports
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 export default {
-  components:{
-    'ninjas':Ninjas
-  },
-  name: 'app',
-  data () {
-    return {
-      title: 'Ninja App',
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ],
+          title: 'Vue Wizards'
+        }
     }
-  }
 }
 </script>
 
-<style scoped>
-
-  h1{
-    color: brown;
-  }
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
 </style>
