ea2c5691db1d31f8f0f3819855816db36e5a81b4
Jonas Castro <jonascastro128@gmail.com>
2020-08-19 19:31:03 -0300

    Dynamic Components

---
+++ #28 - Dynamic Components/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <keep-alive>
+            <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component = 'form-one'">Show form one</button>
+        <button v-on:click="component = 'form-two'">Show form two</button>
+    </div>
+</template>
+
+<script>
+// Imports
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
+export default {
+    components: {
+        'form-one': formOne,
+        'form-two': formTwo
+    },
+    data () {
+        return {
+            component: 'form-one'
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
