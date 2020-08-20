21189f2496c965c7e49440abf04d34493c51f81e
Jonas Castro <jonascastro128@gmail.com>
2020-08-20 11:21:16 -0300

    #30 - Checkbox Binding

--- #29 - Input Binding (Creating a blog, part 1)/src/components/addBlog.vue
+++ #29 - Input Binding (Creating a blog, part 1)/src/components/addBlog.vue
@@ -6,12 +6,27 @@
             <input type="text" v-model.lazy="blog.title" required />
             <label>Blog Content:</label>
             <textarea v-model.lazy.trim="blog.content"></textarea>
+            <div id="checkboxes">
+                <p>Blog Categories:</p>
+                <label>Ninjas</label>
+                <input type="checkbox" value="ninjas" v-model="blog.categories" />
+                <label>Wizards</label>
+                <input type="checkbox" value="wizards" v-model="blog.categories" />
+                <label>Mario</label>
+                <input type="checkbox" value="mario" v-model="blog.categories" />
+                <label>Cheese</label>
+                <input type="checkbox" value="cheese" v-model="blog.categories" />
+            </div>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
             <p>Blog title: {{ blog.title }}</p>
             <p>Blog content:</p>
             <p style="white-space: pre">{{ blog.content }}</p>
+            <p>Blog Categories:</p>
+            <ul>
+                <li v-for="category in blog.categories">{{ category }}</li>
+            </ul>
         </div>
     </div>
 </template>
@@ -23,7 +38,8 @@ export default {
         return {
             blog: {
                 title: '',
-                content: ''
+                content: '',
+                categories: []
             }
         }
     },
@@ -56,5 +72,12 @@ input[type="text"], textarea{
 }
 h3{
     margin-top: 10px;
+}
+#checkboxes input{
+    display: inline-block;
+    margin-right: 10px;
+}
+#checkboxes label{
+    display: inline-block;
 }
 </style>
