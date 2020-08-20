06d0e1c3fc2f2859be6b4196ee24120a8f9a26fe
Jonas Castro <jonascastro128@gmail.com>
2020-08-20 11:31:17 -0300

    #31 - Select Box Binding

--- #29 - Input Binding (Creating a blog, part 1)/src/components/addBlog.vue
+++ #29 - Input Binding (Creating a blog, part 1)/src/components/addBlog.vue
@@ -17,6 +17,10 @@
                 <label>Cheese</label>
                 <input type="checkbox" value="cheese" v-model="blog.categories" />
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option v-for="author in authors">{{ author }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
@@ -27,6 +31,7 @@
             <ul>
                 <li v-for="category in blog.categories">{{ category }}</li>
             </ul>
+            <p>Author: {{ blog.author }}</p>
         </div>
     </div>
 </template>
@@ -39,8 +44,10 @@ export default {
             blog: {
                 title: '',
                 content: '',
-                categories: []
-            }
+                categories: [],
+                author: ''
+            },
+            authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
         }
     },
     methods: {
@@ -79,5 +86,6 @@ h3{
 }
 #checkboxes label{
     display: inline-block;
+    margin-top: 0;
 }
 </style>
