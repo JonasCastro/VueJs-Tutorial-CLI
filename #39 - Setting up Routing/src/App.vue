13d827417b2961aff954957ec1ce814b3145c12f
Jonas Castro <jonascastro128@gmail.com>
2020-08-21 16:33:54 -0300

    #39 - Setting up Routing

---
+++ #39 - Setting up Routing/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+// Imports
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
+export default {
+    components: {
+        'show-blogs': showBlogs,
+        'list-blogs': listBlogs
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
